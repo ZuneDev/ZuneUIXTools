29c5d85c49a73cd4830693f7c8a32aeafc633462
Joshua Askharoun <jjask7@gmail.com>
2021-04-04 01:15:52 -0500

    Added basic edit-and-run functionality to IDE

--- ZuneUIXTools/MainWindow.xaml.cs
+++ ZuneUIXTools/MainWindow.xaml.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.Iris;
+﻿using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using ICSharpCode.AvalonEdit.Rendering;
+using Microsoft.Iris;
 using Microsoft.Iris.Markup;
 using Microsoft.Iris.Session;
 using System;
 using System.Collections;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -11,6 +16,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -25,6 +31,9 @@ namespace ZuneUIXTools
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string DocumentPath { get; set; }
+        public string UIRoot { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -34,51 +43,111 @@ namespace ZuneUIXTools
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            Thread newWindowThread = new Thread(new ThreadStart(TestCompile));
-            newWindowThread.SetApartmentState(ApartmentState.STA);
-            newWindowThread.IsBackground = true;
-            newWindowThread.Start();
+            
         }
 
-        private void TestCompile()
+        private void BuildAndRun()
         {
             MarkupSystem.Startup(true);
             ErrorManager.OnErrors += (IList errors) => {
                 foreach (object obj in errors)
                 {
-                    var err = obj as ErrorRecord;
-                    if (err == null) continue;
+                    if (!(obj is ErrorRecord err)) continue;
 
 #if DEBUG
                     System.Diagnostics.Debug.WriteLine(err.Message);
-                    System.Diagnostics.Debugger.Break();
+                    //System.Diagnostics.Debugger.Break();
 #else
                     Console.WriteLine(err.Message);
 #endif
+
+                    Dispatcher.Invoke(() =>
+                    {
+                        ErrorPanel.Children.Add(new TextBlock
+                        {
+                            Text = $"Ln {err.Line}, Col {err.Column}: {err.Message}",
+                            Margin = new Thickness(0, 0, 0, 4)
+                        });
+
+                        // Highlight the error in the code editor
+                        int offset = textEditor.Document.GetOffset(err.Line, err.Column);
+                        textEditor.SelectionStart = offset;
+                        //textEditor.SelectionLength = err.
+                        //var anchor = textEditor.Document.CreateAnchor(offset);
+                        //textEditor.Document.GetLineByOffset(offset)
+                    });
                 }
             };
 
-            string testDir = @"D:\Repos\yoshiask\ZuneUIXTools\test\";
-            string testName = "text";
-            string sourceFile = System.IO.Path.Combine(testDir, testName) + ".uix";
-            string compiledFile = System.IO.Path.Combine(testDir, testName) + ".uib";
-
+            string compiledFile = System.IO.Path.ChangeExtension(DocumentPath, "uib");
             bool isSuccess = MarkupCompiler.Compile(
                 new[]
                 {
                     new CompilerInput()
                     {
-                        SourceFileName = sourceFile,
+                        SourceFileName = DocumentPath,
                         OutputFileName = compiledFile
                     }
                 },
                 new CompilerInput()
             );
 
-            Microsoft.Iris.Application.Initialize();
-            Microsoft.Iris.Application.Window.SetBackgroundColor(new WindowColor(80, 0, 0));
-            Microsoft.Iris.Application.Window.RequestLoad("file://" + compiledFile + "#TextDisplay");
-            Microsoft.Iris.Application.Run();
+            try
+            {
+                Microsoft.Iris.Application.Initialize();
+            }
+            catch { }
+
+            try
+            {
+                Microsoft.Iris.Application.Window.SetBackgroundColor(new WindowColor(0xE6, 0xE6, 0xE6));
+                Microsoft.Iris.Application.Window.RequestLoad("file://" + compiledFile + (string.IsNullOrEmpty(UIRoot) ? string.Empty : "#" + UIRoot));
+                Microsoft.Iris.Application.Run();
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.Invoke(() =>
+                    ErrorPanel.Children.Add(new TextBlock
+                    {
+                        Text = $"Load Failed: {ex.Message}",
+                        Margin = new Thickness(0, 0, 0, 4)
+                    })
+                );
+            }
+
+            //Microsoft.Iris.Application.Window.
+            //Microsoft.Iris.Application.Shutdown();
+
+            // This code is run AFTER the Iris window is closed
+        }
+
+        private void Build_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(DocumentPath))
+                return;
+
+            // Save changes before running
+            textEditor.Save(DocumentPath);
+            // Set UI root
+            UIRoot = UIRootBox.Text;
+
+            Thread newWindowThread = new Thread(new ThreadStart(BuildAndRun));
+            newWindowThread.SetApartmentState(ApartmentState.STA);
+            newWindowThread.IsBackground = true;
+            newWindowThread.Start();
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Microsoft Iris UI (*.uix)|*.uix|XML files (*.xml)|*.xml"
+            };
+            if (openFileDialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+
+            DocumentPath = openFileDialog.FileName;
+            textEditor.Load(DocumentPath);
         }
     }
 }
