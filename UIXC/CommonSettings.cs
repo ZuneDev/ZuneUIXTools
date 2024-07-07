af0d7291aaab467490d6b70e93ff21e9299a221a
Yoshi Askharoun <jjask7@gmail.com>
2024-07-07 15:36:59 -0500

    Add default values for common compiler settings

--- UIXC/CommonSettings.cs
+++ UIXC/CommonSettings.cs
@@ -14,7 +14,7 @@ public abstract class CompilerSettings : CommonSettings
 {
     [Description("The directory to output to.")]
     [CommandOption("-o|--output <outputDir>")]
-    public string? OutputDir { get; init; } = Environment.CurrentDirectory;
+    public required string OutputDir { get; init; } = Environment.CurrentDirectory;
 
     [Description("The shared binary data table to use.")]
     [CommandOption("-t|--dataTable")]
@@ -22,13 +22,13 @@ public abstract class CompilerSettings : CommonSettings
 
     [Description("Directories to search for imported files in.")]
     [CommandOption("-I <VALUES>")]
-    public required string[] IncludeDirectories { get; init; }
+    public required string[] IncludeDirectories { get; init; } = [];
 
     [Description("External assemblies to load.")]
     [CommandOption("-A <VALUES>")]
-    public required string[] Assemblies { get; init; }
+    public required string[] Assemblies { get; init; } = [];
 
     [Description("Redirects imports.")]
     [CommandOption("--ir <VALUES>")]
-    public required string[] ImportRedirects { get; init; }
+    public required string[] ImportRedirects { get; init; } = [];
 }
