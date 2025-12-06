a702a08064e6dfbd21ed354ca41dbef55d226b7e
Joshua "Yoshi" Askharoun <jjask7@gmail.com>
2025-12-06 01:48:45 -0600

    Stub threads, attach, and launch handlers
    Required for VS Code

---
+++ DebugAdapter/UIX.DebugAdapter.Server/Handlers/AttachHandler.cs
@@ -0,0 +1,18 @@
+﻿using OmniSharp.Extensions.DebugAdapter.Protocol.Requests;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Iris.DebugAdapter.Server.Handlers;
+
+internal class AttachHandler : IAttachHandler, ILaunchHandler
+{
+    public Task<LaunchResponse> Handle(LaunchRequestArguments request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(new LaunchResponse());
+    }
+
+    public Task<AttachResponse> Handle(AttachRequestArguments request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(new AttachResponse());
+    }
+}
---
+++ DebugAdapter/UIX.DebugAdapter.Server/Handlers/ThreadsHandler.cs
@@ -0,0 +1,26 @@
+﻿using OmniSharp.Extensions.DebugAdapter.Protocol.Requests;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using DapThread = OmniSharp.Extensions.DebugAdapter.Protocol.Models.Thread;
+
+namespace Microsoft.Iris.DebugAdapter.Server.Handlers;
+
+internal class ThreadsHandler : ThreadsHandlerBase
+{
+    public override Task<ThreadsResponse> Handle(ThreadsArguments request, CancellationToken cancellationToken)
+    {
+        List<DapThread> threads = [
+            new DapThread
+            {
+                Id = 0,
+                Name = "Main Thread"
+            }
+        ];
+
+        return Task.FromResult(new ThreadsResponse
+        {
+            Threads = threads
+        });
+    }
+}
